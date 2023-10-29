af2f46aa47f65fbba7717cf872dce4a92e0a700b
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-29 16:19:02 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import Navbar from './Components/Navbar'
+import {Men, Women, Home, Brands, Products} from './pages'
 
 function App() {
  return(
   <ChakraProvider>
     <BrowserRouter>
-      <h1>yahya</h1>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element = {<Home />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   </ChakraProvider>
  )
---
+++ src/Pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Men } from './Men';
+export { default as Women } from './Women';
+export { default as Home } from './Home';
+export { default as Brands } from './Brands';
+export { default as Products } from './Products';
