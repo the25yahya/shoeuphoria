32a9c5fd8975eafd6341cb9dfbe46a9726ba7a40
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-29 16:02:05 +0100

    added packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function App() {
  return(
-  <BrowserRouter>
-    <h1>yahya</h1>
-  </BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
+      <h1>yahya</h1>
+    </BrowserRouter>
+  </ChakraProvider>
  )
 }
 
