c1a9d98dbeec290e4ddd601edeb4d790ceb3df10
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-30 16:55:07 +0100

    added navigation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { FaShoppingBag, FaUserAlt } from 'react-icons/fa'
 import SearchForm from './SearchFrom'
+import { Link } from 'react-router-dom';
+import Men from '../Pages/Men'
+import Women from '../Pages/Women'
+import Brands from '../Pages/Brands'
+import Products from '../Pages/Products'
+import Home from '../pages/Home';
 
 
 const Navbar = () => {
@@ -8,10 +14,11 @@ const Navbar = () => {
     <nav className='flex justify-around p-10 px-20 items-center'>
      <p className='font-semibold text-xl'>SHOE<span className='text-orange-500'>UPHORIA</span></p>
      <div className='flex items-center font-semibold'>
-      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Men</p>
-      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Women</p>
-      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Categories</p>
-      <p className='pr-20 hover:text-orange-500 cursor-pointer transition'>Brands</p>
+     <Link to='/'><p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Home</p></Link>
+      <Link to='/Men'><p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Men</p></Link>
+      <Link to='/Women'><p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Women</p></Link>
+      <Link to='/Products'><p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Products</p></Link>
+      <Link to='/Brands'><p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Brands</p></Link>
       <SearchForm />
        <div className='px-3 pr-8'>
          <FaShoppingBag />
