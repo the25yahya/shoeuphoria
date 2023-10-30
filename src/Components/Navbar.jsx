867c0ea1cf44fcfe5490513d0f0ccc1c3cec270e
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-30 15:44:53 +0100

    created navbar layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FaShoppingBag, FaUserAlt } from 'react-icons/fa'
+import SearchForm from './SearchFrom'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-around p-10 px-20 items-center'>
+     <p className='font-semibold text-xl'>SHOE<span className='text-orange-500'>UPHORIA</span></p>
+     <div className='flex items-center font-semibold'>
+      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Men</p>
+      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Women</p>
+      <p className='pr-8 hover:text-orange-500 cursor-pointer transition'>Categories</p>
+      <p className='pr-20 hover:text-orange-500 cursor-pointer transition'>Brands</p>
+      <SearchForm />
+       <div className='px-3 pr-8'>
+         <FaShoppingBag />
+       </div>
+       <div>
+         <FaUserAlt />
+       </div>
+     </div>
+    </nav>
   )
 }
 
